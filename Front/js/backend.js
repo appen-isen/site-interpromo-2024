21539e81a31422f13a6d7ce4dfdb982ef54da4a2
Félix MARQUET <fpmovie4@gmail.com>
2023-10-31 19:59:43 +0100

    Create the authentification (back and front V6 only for the moment)

--- Front/js/backend.js
+++ Front/js/backend.js
@@ -1,2 +1,47 @@
 console.log("Backend start loading...");
+import PocketBase from './pocketbase.es.mjs'
+
+const pb = new PocketBase("http://[2a01:e0a:83b:6aa0:4c08:83ff:fe64:8ee0]:8090");
+
+const loginButton = document.querySelector('button[type="submit"]');
+
+loginButton.addEventListener('click', async function(event) {
+    event.preventDefault();
+
+    const loginID = document.getElementById('LoginID').value;
+    const loginPassword = document.getElementById('LoginPassword').value;
+
+    try {
+        const authData = await pb.collection('users').authWithPassword(loginID, loginPassword);
+        console.log('Authentification réussie :', authData);
+
+        if (pb.authStore.isValid) {
+            console.log('Le token est valide.');
+            window.location.href = "arbitrage.html";
+        } else {
+            console.log('Le token n\'est pas valide.');
+        }
+    } catch (error) {
+        console.error('Erreur d\'authentification :', error);
+    }
+});
+
+
+//Check if the user is already logged in
+if (pb.authStore.isValid) {
+    const logoutButton = document.getElementById('disconect');
+    logoutButton.addEventListener('click', async function(event) {
+        event.preventDefault();
+        pb.authStore.clear();
+        window.location.href = "login.html";
+    });
+}
+
+if(window.location.href.includes("arbitrage.html") || window.location.href.includes("arbimatch.html")){
+    if(pb.authStore.isValid === false){
+        window.location.href = "login.html";
+    }
+}
+
+
 console.log("Backend loaded!");
