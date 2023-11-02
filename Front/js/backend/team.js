b6f1cb5ef123996e234f9540b4e3131139ae5aac
Allan Cueff <allan.cueff@protonmail.com>
2023-11-03 00:43:58 +0100

    clean code + bootstrap lists

--- Front/js/backend/team.js
+++ Front/js/backend/team.js
@@ -33,173 +33,147 @@ const matchList = await pb.collection('match').getFullList({
     expand: 'team1,team2,sport',
 });
 
+function getTeamClassement(equipe){
+    switch(equipe.expand.sport.name){
+        case "football":
+            classFootballList.forEach(classe => {
+                if (classe.expand.team.name === equipe.name) {
+                    return classe.classement;
+                }
+            });
+        case "handball":
+            classHandballList.forEach(classe => {
+                if (classe.expand.team.name === equipe.name) {
+                    return classe.classement;
+                }
+            });
+        case "volleyball":
+            classVoleyballList.forEach(classe => {
+                if (classe.expand.team.name === equipe.name) {
+                    return classe.classement;
+                }
+            });
+        case "basketball":
+            classBasketballList.forEach(classe => {
+                if (classe.expand.team.name === equipe.name) {
+                    return classe.classement;
+                }
+            });
+        case "badminton":
+            //return ``
+        case "defi enduro":
+            classDefiList.forEach(classe => {
+                if (classe.expand.team.name === equipe.name) {
+                    return classe.classement;
+                }
+            });
+    }
+    return 0;
+}
+
+function getTeamRow(equipe){
+    let result =  `
+    <li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+            <div class="fw-bold">${equipe.expand.sport.name}</div>
+            ${equipe.name}
+        </div>`
+    let classement = getTeamClassement(equipe);
+    if(classement != 0){
+        result += `<span class="badge bg-primary rounded-pill">${classement}/...</span>`
+    }
+    result += `</li>`
+    return result
+}
+
+function getPromoCard(promo, teamsList){
+    let cardHtml = `
+    <div class="card my-3">
+        <div class="card-header text-center bg-light-subtle text-emphasis-light">${promo.name}</div>
+        <ul class="list-group list-group-flush list-group-item">
+    `
+    teamsList.forEach(equipe => {
+        cardHtml += getTeamRow(equipe)
+    })
+    cardHtml += `</ul>`
+    const nextMatch = matchList.find(match => match.expand.team1.promo === promo.id || match.expand.team2.promo === promo.id);
+    if(nextMatch){
+        const time_start = new Date(nextMatch.heure_debut);
+        cardHtml += `<div class="card-footer bg-light-subtle text-emphasis-light">Prochain match : ${nextMatch.expand.team1.name} vs ${nextMatch.expand.team2.name} à ${time_start.getHours()}h${time_start.getMinutes()}</div>`    
+    }
+    cardHtml += `</div>`
+    return cardHtml
+}
+
+function getSportRow(equipe){
+    console.log(equipe)
+    let members = `Membres : ${equipe.capitaine} ,`;
+    equipe.membres.forEach(membre => {
+        members += membre.name + " ,";
+    });
+    console.log(members)
+    members = members.slice(0, -2)
+    let result =  `
+    <li class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="ms-2 me-auto">
+            <div class="fw-bold">${equipe.expand.sport.name}</div>
+            ${members}
+        </div>`
+    let classement = getTeamClassement(equipe);
+    if(classement != 0){
+        result += `<span class="badge bg-primary rounded-pill">${classement}/...</span>`
+    }
+    result += `</li>`
+    return result
+}
+
+function getTeamCard(teamBySport){
+    let equipe = teamBySport['team']
+    let cardHtml = `
+    <div class="card my-3">
+        <div class="card-header text-center bg-light-subtle text-emphasis-light">${equipe.name}(${equipe.expand.promo.name})</div>
+        <ul class="list-group list-group-flush list-group-item">
+    `
+    for(const [sportName, team] of Object.entries(teamBySport).filter(([sportName, team]) => sportName != "team")){
+        cardHtml += getSportRow(team)
+    }
+    cardHtml += `</ul>`
+    const nextMatch = matchList.find(match => match.expand.team1.name === equipe.name || match.expand.team2.name === equipe.name);
+    if(nextMatch){
+        const time_start = new Date(nextMatch.heure_debut);
+        cardHtml += `<div class="card-footer bg-light-subtle text-emphasis-light">Prochain match : ${nextMatch.expand.team1.name} vs ${nextMatch.expand.team2.name} à ${time_start.getHours()}h${time_start.getMinutes()}</div>`    
+    }
+    cardHtml += `</div>`
+    return cardHtml
+}
+
 const promoCardContainer = document.getElementById("promoCardContainer");
+const teamCardContainer = document.getElementById("teamCardContainer");
+
+const promoTeams = {} // Object key = promo name, value = array of promo teams objects
+const teamSports = {} // Object key = team name, value = Object key = sport name, value = team object
+
+EquipeList.forEach(equipe => {
+    if(!(equipe.expand.promo.name in promoTeams)){
+        promoTeams[equipe.expand.promo.name] = [];
+    }
+    promoTeams[equipe.expand.promo.name].push(equipe);
+    if(!(equipe.name in teamSports)){
+        teamSports[equipe.name] = {'team': equipe};
+    }
+    teamSports[equipe.name][equipe.expand.sport.name] = equipe;
+})
 
 //Affichage des équipes par promo
 PromoList.forEach(promo => {
-    const card = document.createElement('div');
-    card.classList.add('card', 'my-3');
-    //Affichage du nom de la promo
-    const cardHeader = document.createElement('div');
-    cardHeader.classList.add('card-header', 'text-center', 'bg-light-subtle', 'text-emphasis-light');
-    cardHeader.innerHTML = promo.name;
-    card.appendChild(cardHeader);
-    //Affichage des équipes de la promo
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body', 'bg-light-subtle', 'text-emphasis-light');
-    const cardText = document.createElement('p');
-    //Pour chaque équipe, verif si elle est dans la promo et affichage du sport et du nom de l'équipe et du classement
-    EquipeList.forEach(equipe => {
-        if(equipe.expand.promo.name === promo.name){
-            if(equipe.expand.sport.name === "basketball") {
-                //Affichage du classement
-                let classement = 0;
-                classBasketballList.forEach(classe => {
-                    if(classe.expand.team.name === equipe.name){
-                        classement = classe.classement;
-                    }
-                });
-                card.innerHTML += "Basketball : " + equipe.name + " : " + classement + "e <br> ";
-            }
-            else if(equipe.expand.sport.name === "football") {
-                let classement = 0;
-                classFootballList.forEach(classe => {
-                    if(classe.expand.team.name === equipe.name){
-                        classement = classe.classement;
-                    }
-                });
-                cardText.innerHTML += "Football : " + equipe.name + " : " + classement + "e <br> ";
-            }
-            else if(equipe.expand.sport.name === "handball") {
-                let classement = 0;
-                classHandballList.forEach(classe => {
-                    if(classe.expand.team.name === equipe.name){
-                        classement = classe.classement;
-                    }
-                });
-                cardText.innerHTML += "Handball : " + equipe.name + " : " + classement + "e <br> ";
-            }
-            else if(equipe.expand.sport.name === "volleyball") {
-                let classement = 0;
-                classVoleyballList.forEach(classe => {
-                    if(classe.expand.team.name === equipe.name){
-                        classement = classe.classement;
-                    }
-                });
-                cardText.innerHTML += "Volleyball : " + equipe.name + " : " + classement + "e <br> ";
-            }
-            else if(equipe.expand.sport.name === "defi enduro"){
-                let classement = 0;
-                classDefiList.forEach(classe => {
-                    if(classe.expand.team.name === equipe.name){
-                        classement = classe.classement;
-                    }
-                });
-                cardText.innerHTML += "Défi Enduro : " + equipe.name + " : " + classement + "e <br> ";
-            }
-            else if(equipe.expand.sport.name === "badminton"){
-                cardText.innerHTML += "Badminton : " + equipe.name + " : " + "BACK A FAIRE" + "e <br> ";
-            }
-        }
-    });
-    cardBody.appendChild(cardText);
-    card.appendChild(cardBody);
-    promoCardContainer.appendChild(card);
+    if(!(promo.name in promoTeams)){
+        promoTeams[promo.name] = []
+    }
+    promoCardContainer.insertAdjacentHTML("beforeend", getPromoCard(promo, promoTeams[promo.name]));
 });
 
-
-
-const teamCardContainer = document.getElementById("teamCardContainer");
-EquipeList.forEach(equipe => {
-    const card = document.createElement('div');
-    card.classList.add('card', 'my-3');
-    //Affichage du nom de l'équipe
-    const cardHeader = document.createElement('div');
-    cardHeader.classList.add('card-header', 'text-center', 'bg-light-subtle', 'text-emphasis-light');
-    cardHeader.innerHTML = equipe.name;
-    //Affichage des infos de l'équipe
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body', 'bg-light-subtle', 'text-emphasis-light');
-    //Affichage du sport
-    const cardText1 = document.createElement('h5');
-    cardText1.classList.add('card-title');
-    cardText1.innerHTML = "Sport : " + equipe.expand.sport.name;
-    //Affichage de la promo
-    const cardText2 = document.createElement('p');
-    cardText2.classList.add('card-text');
-    cardText2.innerHTML = "Promo : " + equipe.expand.promo.name;
-    //Affichage des membres
-    const cardMember = document.createElement('p');
-    cardMember.classList.add('card-text');
-    cardMember.innerHTML = "Membres : " + equipe.capitaine + " ,";
-    equipe.expand.membres.forEach(membre => {
-        cardMember.innerHTML += " " + membre.name + " ,";
-    });
-    //Affichage du classement
-    const cardClassement = document.createElement('p');
-    cardClassement.classList.add('card-text');
-    let classement = 0;
-    if(equipe.expand.sport.name === "basketball") {
-        classBasketballList.forEach(classe => {
-            if(classe.expand.team.name === equipe.name){
-                classement = classe.classement;
-            }
-        });
-    }
-    else if(equipe.expand.sport.name === "football") {
-        classFootballList.forEach(classe => {
-            if(classe.expand.team.name === equipe.name){
-                classement = classe.classement;
-            }
-        });
-    }
-    else if(equipe.expand.sport.name === "handball") {
-        classHandballList.forEach(classe => {
-            if(classe.expand.team.name === equipe.name){
-                classement = classe.classement;
-            }
-        });
-    }
-    else if(equipe.expand.sport.name === "volleyball") {
-        classVoleyballList.forEach(classe => {
-            if(classe.expand.team.name === equipe.name){
-                classement = classe.classement;
-            }
-        });
-    }
-    else if(equipe.expand.sport.name === "defi enduro"){
-        classDefiList.forEach(classe => {
-            if(classe.expand.team.name === equipe.name){
-                classement = classe.classement;
-            }
-        });
-    }
-    else if(equipe.expand.sport.name === "badminton"){
-        classement = "BACK A FAIRE";
-    }
-    cardClassement.innerHTML += " Classement : " + classement +"e";
-    //Suppression de la dernière virgule
-    cardMember.innerHTML = cardMember.innerHTML.slice(0, -1);
-    //Suppression du dernier espace
-    cardMember.innerHTML = cardMember.innerHTML.slice(0, -1);
-    //Affichage du prochain match
-    const cardFooter = document.createElement('div');
-    cardFooter.classList.add('card-footer', 'bg-light-subtle', 'text-emphasis-light');
-    const nextMatch = matchList.find(match => match.expand.team1.name === equipe.name || match.expand.team2.name === equipe.name);
-    const time_start = new Date(nextMatch.heure_debut);
-    cardFooter.innerHTML = "Prochain match : " + nextMatch.expand.team1.name + " vs " + nextMatch.expand.team2.name + " à " + time_start.getHours() + "h" + time_start.getMinutes();
-
-    cardBody.appendChild(cardText1);
-    cardBody.appendChild(cardText2);
-    cardBody.appendChild(cardClassement);
-    cardBody.appendChild(cardMember);
-
-    card.appendChild(cardHeader);
-    card.appendChild(cardBody);
-    card.appendChild(cardFooter);
-
-    teamCardContainer.appendChild(card);
-});
-
+Object.values(teamSports).forEach(teamBySport => {
+    teamCardContainer.insertAdjacentHTML("beforeend", getTeamCard(teamBySport))
+})
 
 console.log("Backend team loaded!");
