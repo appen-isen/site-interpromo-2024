ce06f5de8022f8d7aa2759891c12cd3791daaba3
Félix MARQUET <fpmovie4@gmail.com>
2024-11-13 19:57:59 +0100

    Try to fix

--- Front/js/backend/login.js
+++ Front/js/backend/login.js
@@ -3,6 +3,8 @@ import PocketBase from '../pocketbase.es.mjs'
 
 const pb = new PocketBase("https://pocketbase.appen.fr");
 
+pb.autoCancellation(false);
+
 //Connexion
 if (window.location.href.includes("login.html")) {
     const loginButton = document.querySelector('button[type="submit"]');
